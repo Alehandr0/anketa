85e6531b1c726eb6ed8bbd974c6f1d59ade5da05
Alex <Alehandr0@users.noreply.github.com>
2016-06-23 22:08:23 +0300

    Add files via upload

---
+++ page/3.php
@@ -0,0 +1,15 @@
+<?
+$rendere=array("title" => "Создать анкету",
+"h2" => "Создать анкету по линии HR, page = {$_GET["page"]}",
+"ul" => array(0 => 2,
+1 => array("href" => "1.0.0.0", "li" => "Назад"),
+2 => array("href" => "0.0.0.0", "li" => "В начало")),
+//"lib" => array(0 => 1, 1 => 1),
+"content" => "Добавить форму",
+"form" => array(
+0 => array(3, "page" => 7, "header" => 0, "body" => 2, "footer" => 0),
+1 => array(1, 1),
+2 => array(2, 2),
+3 => array(3, 3),
+),
+"kap4a" => 1);
